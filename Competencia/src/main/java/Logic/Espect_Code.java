cf95566a1dff998799f90362b2613345898af7bc
alinesilmer <167638880+alinesilmer@users.noreply.github.com>
2024-06-24 17:24:20 -0300

    Add files via upload

---
+++ Competencia/src/main/java/Logic/Espect_Code.java
@@ -0,0 +1,31 @@
+package Logic;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hecto
+ */
+public class Espect_Code {
+    public static void main(String[] args){
+        
+    }
+    
+}
+
+class Espectador{
+    private String nombre_espectador;
+    
+    public Espectador(){
+     
+}
+    
+    //Seleccionar una CATEGORIA
+   //mostrar RANKING_GRAL por categoría
+    //mostrar RANKING_ETAPAS
+    //mostrar LISTA_PARTICIP
+    //
+}
---
+++ Competencia/src/main/java/Logic/registro_Espectador.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logic;
+
+/**
+ *
+ * @author hecto
+ */
+public class registro_Espectador {
+    private String usuario_espectador;
+    private int id_espectador;
+    
+    public registro_Espectador(){
+        
+    }
+    
+    public registro_Espectador(String usuario_espectador, int id_espectador){
+        this.usuario_espectador = usuario_espectador;
+        this.id_espectador = id_espectador;
+    }
+    
+    
+}
